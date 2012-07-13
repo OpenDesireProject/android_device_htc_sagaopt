1bc18c608a6996a2b075ae1e6239e975b8ce9c2b
Nico Kaiser <kaiser.nico@gmail.com>
2012-07-13 14:19:38 +0200

    saga: change permissions xml path to frameworks/native
    Change-Id: I11c6e3f2fc15ed17082eaf7f45fe25868e328dae

--- device.mk
+++ device.mk
@@ -27,8 +27,8 @@ PRODUCT_COPY_FILES += \
     device/htc/saga/ueventd.saga.rc:root/ueventd.saga.rc
 
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/base/native/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/native/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 ## (2) Also get non-open-source GSM-specific aspects if available
 $(call inherit-product-if-exists, vendor/htc/saga/saga-vendor.mk)
