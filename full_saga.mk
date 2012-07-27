a037ef99cad2783ea64fa27d6d561787b5992e58
Alexander zinn <zinnalex@gmail.com>
2012-07-27 06:39:20 -0700

    Torch and CRT

--- full_saga.mk
+++ full_saga.mk
@@ -26,7 +26,7 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
  
 # Camera
 PRODUCT_PACKAGES := \
-    Camera
+    Torch
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
