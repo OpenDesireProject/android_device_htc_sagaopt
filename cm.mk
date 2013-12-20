44022e6196d8623fc52b0b866d6c408f92165e62
Nicholas Flintham <nick@flinny.org>
2013-12-20 22:24:47 +0000

    4.4 update for Saga
    Change-Id: Ied4866bc8ae3a98239fc38f6c9473c15f0295cf1

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_saga BUILD_FINGERPRINT=htc_europe/htc_saga/saga:4.2.2/JDQ39/87995:user/release-keys PRIVATE_BUILD_DESC="4.2.2 Andromadus release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_saga BUILD_FINGERPRINT=htc_europe/htc_saga/saga:4.4.2/KOT49H/87995:user/release-keys PRIVATE_BUILD_DESC="4.4.2 Andromadus release-keys"
 
 PRODUCT_NAME := cm_saga
 PRODUCT_DEVICE := saga
