b2fa668c839126dba0dd825203711ab78ad0e5a4
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-01-03 13:41:30 +0200

    saga: move android.hardware.telephony.gsm.xml to common
    Change-Id: Ib4722decf27ca2f489fe20094181f29833be78be

--- device.mk
+++ device.mk
@@ -100,10 +100,6 @@ PRODUCT_PACKAGES += \
 	lights.saga \
 	sensors.saga
 
-# Permissions
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-
 # Set build date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
