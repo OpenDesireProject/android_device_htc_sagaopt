bff069e4a66e3a0bd85580df6c2821f97f844bc1
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-04-16 01:11:21 +0300

    saga: use halfres bootanimation
    Change-Id: I98182e26b0a4b0d4fdd50f587fda3dd6c099495c

--- full_saga.mk
+++ full_saga.mk
@@ -19,6 +19,7 @@ $(call inherit-product, device/htc/saga/saga.mk)
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_saga
