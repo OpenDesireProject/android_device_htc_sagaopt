dec930499eafa4db793eada17638309bb8a9d254
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-10-23 06:34:21 -0700

    sagaopt: squashed revert of refactoring
    Something here breaks ace boot, it's probably better to fix BT first and then
    proceed with other things to verify that one thing is fixed before breaking
    other stuff.
    
    commit 68438d2cc5fc0a2abad467eb0128319ec8d07f56
    Author: Mustaavalkosta <niko.hyrynsalmi@gmail.com>
    Date:   Sat Oct 22 15:03:08 2016 -0700
    
        sagaopt: include bt_vendor.conf properly
    
    commit 1b8cbead0f23c2dca65cdafe03a6a8f99c4189a1
    Author: kylon <kylonux@gmail.com>
    Date:   Wed Oct 19 16:56:17 2016 +0200
    
        Remove redundant flag
    
        Moved to common
    
    commit 987cba96b69b3bc9a97a22f1fb3b80bc7d6c09a9
    Author: kylon <kylonux@gmail.com>
    Date:   Wed Oct 19 16:38:45 2016 +0200
    
        Refactor sagaopt device tree
    
    commit fa8447dfcb4a8fec77f59ddee34a69600b0a620c
    Author: kylon <kylonux@gmail.com>
    Date:   Tue Oct 18 14:50:08 2016 +0200
    
        Move common flags to msm7x30-common
    
    Change-Id: I9caa529f099327e5430da9e8f4ff488f74425ab2

--- cm.mk
+++ cm.mk
@@ -1,37 +1,20 @@
-# Copyright (C) 2016 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit device configuration
+$(call inherit-product, device/htc/sagaopt/full_saga.mk)
 
 # Inherit common CM configuration
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit from saga device
-$(call inherit-product, device/htc/sagaopt/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_sagaopt
-PRODUCT_DEVICE := sagaopt
-PRODUCT_BRAND := htc_wwe
-PRODUCT_MANUFACTURER := HTC
-PRODUCT_MODEL := HTC DESIRE S
-PRODUCT_GMS_CLIENTID_BASE := android-htc-rev
-PRODUCT_RELEASE_NAME := sagaopt
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=htc_saga \
     BUILD_FINGERPRINT="htc_wwe/htc_saga/saga:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date +%Y%m%d%H%M%S):$(TARGET_BUILD_VARIANT)/test-keys" \
     PRIVATE_BUILD_DESC="htc_saga-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(shell date +%Y%m%d%H%M%S) test-keys"
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_sagaopt
+PRODUCT_GMS_CLIENTID_BASE := android-htc-rev
+
+PRODUCT_BOOTANIMATION := device/htc/msm7x30-common/rootdir/system/media/bootanimation.zip
+
+# Release name
+PRODUCT_RELEASE_NAME := sagaopt
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
 add_lunch_combo cm_sagaopt-userdebug
+add_lunch_combo cm_sagaopt-eng
