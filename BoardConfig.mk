ddedd1e0a6c67a8120f91434463684112de12a5a
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-10-24 12:25:28 -0700

    sagaopt: keep kernel flags in devices
    Base address is different for ace and saga and this stuff rarely changes
    anyways and is better to be device specific just like the kernel is.
    
    Change-Id: Ia20364cb540f6087975651a78e0a1050987811fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,11 @@ TARGET_OTA_ASSERT_DEVICE := sagaopt
 
 # Kernel
 TARGET_KERNEL_CONFIG := saga_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
+BOARD_KERNEL_CMDLINE := no_console_suspend=1  androidboot.selinux=permissive androidboot.hardware=htc7x30
+BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88
+BOARD_KERNEL_BASE := 0x04400000
+BOARD_KERNEL_PAGE_SIZE := 4096
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/sagaopt/bluetooth/include
